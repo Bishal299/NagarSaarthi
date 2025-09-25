5564e7433bf0e92098e4ddec87ccec274d148225
AdyaArchita <adyaarchitapattnaik@gmail.com>
2025-09-25 16:10:01 +0530

    Deploying

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home/LandingPage";
 import Login from "./pages/Login/LoginPage";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import CitizenProfile from "./pages/Citizen/CitizenProfile";
+import AboutUs from "./pages/AboutUs"; 
+import FAQ from "./pages/FAQ"; 
+
 import ReportIssue from "./pages/ReportIssue/ReportIssue";
 import { NagarSaarthiChatbot } from "./pages/Home/NagarSaarthiChatbot";
 
@@ -11,19 +14,26 @@ function AppWrapper() {
   const location = useLocation();
   const [chatOpen, setChatOpen] = useState(false);
 
-  // Don't show chatbot or floating button on login page
-  const showChat = location.pathname !== "/login";
+  // Don't show chatbot or floating button on login and FAQs page
+  const showChat = location.pathname !== "/login" && location.pathname !== "/faqs";
+
 
   return (
+
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<AdminDashboard />} /> {/* Updated to match navbar */}
-        <Route path="/citizen" element={<CitizenProfile />} />
-        <Route path="/reportissue" element={<ReportIssue />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/dashboard" element={<AdminDashboard />} />
+  <Route path="/citizen" element={<CitizenProfile />} />
+  <Route path="/reportissue" element={<ReportIssue />} />
+  <Route path="/about" element={<AboutUs />} />
+  <Route path="/faqs" element={<FAQ />} />   
+
+  <Route path="*" element={<Navigate to="/" />} />
+</Routes>
+
+
 
       {showChat && (
         <>
